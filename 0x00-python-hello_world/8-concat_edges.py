fa1d6233b54dccde5f17560978ba36544cdf9756
thatiphele <thatayaonephele@gmail.com>
2023-08-10 12:51:18 -0700

    Hot Fix

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "{} {}".format(str[39:66], "with Python")
+str = "{} {}".format(str[39:66], "with Python\n")
 print(str)
