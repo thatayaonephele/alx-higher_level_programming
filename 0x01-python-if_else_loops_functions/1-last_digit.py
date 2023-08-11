463771b89cc33d0d120bffafe033e80239870518
thatiphele <thatayaonephele@gmail.com>
2023-08-11 12:44:25 -0700

    1-last_digit.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+string_num = str(number)
+last_digit = string_num[-1]
+
+msg_pt1 = "Last digit of {} is {}".format(number, last_digit)
+if (int(last_digit) > 5):
+    print("{} {}".format(msg_pt1, "and is greater than 5"))
+elif (int(last_digit) == 5):
+    print("{} {}".format(msg_pt1, "and is 0"))
+if ((int(last_digit) < 5 and (int(last_digit) != 0))):
+    print("{} {}".format(msg_pt1, "and is less than 6 and not 0"))
