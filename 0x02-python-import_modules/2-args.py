9914b262f6202095535dea1a1d2782aeb8da5ed1
thatiphele <thatayaonephele@gmail.com>
2023-08-11 23:51:01 -0700

    Hot Fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
 
+if __name__ == "__main__":
+    import sys
 
-x = (len(sys.argv)) + (-1)
-if x == 1:
-    print("1 argument: ")
-elif x == 0:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(x))
-for y in range(x):
-    print("{}: {}".format(y + 1, sys.argv[y + 1]))
+    x = (len(sys.argv)) + (-1)
+    if x == 1:
+        print("1 argument: ")
+    elif x == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(x))
+    for y in range(x):
+        print("{}: {}".format(y + 1, sys.argv[y + 1]))
