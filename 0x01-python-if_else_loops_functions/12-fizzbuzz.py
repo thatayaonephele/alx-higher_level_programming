62e58f7275a8b418973396fe5737a8af6274ad3b
thatiphele <thatayaonephele@gmail.com>
2023-08-14 05:21:28 -0700

    Hot Fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizz", sep=" ")
         elif (x % 5 == 0 and x % 3 != 0):
             print("Buzz", sep=" ")
-        elif (int(x) % 15 == 0):
+        elif (x % 3 == 0 and x % 5 == 0):
             print("FizzBuzz", sep=" ")
         else:
-            print("{:02d}".format(x))
+            print(x, sep=" ")
