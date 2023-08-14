0dffd2310c8a9537f969a16f8cceffaa14dbf850
thatiphele <thatayaonephele@gmail.com>
2023-08-14 05:01:30 -0700

    print 1-100 sep by space

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1, 101):
+        if (int(x) % 3 == 0):
+            print("Fizz", sep=" ")
+        elif (int(x) % 5 == 0):
+            print("Buzz", sep=" ")
+        elif (int(x) % 15 == 0):
+            print("FizzBuzz", sep=" ")
+        else:
+            print(x)
