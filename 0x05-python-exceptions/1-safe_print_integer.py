5539d99704e7fa89a6545fe5789172355fc5140e
thatiphele <thatayaonephele@gmail.com>
2023-08-28 14:56:30 -0700

    Hot Fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
 
-    x = value
     try:
-        print("{:d}".format(x))
+        print("{:d}".format(value))
         return (True)
     except ValueError:
         return (False)
