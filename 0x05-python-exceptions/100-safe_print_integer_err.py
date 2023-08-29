f8dbc27c2a49645b369b4cd2c72ad5bf2bc5a6de
thatiphele <thatayaonephele@gmail.com>
2023-08-28 18:33:54 -0700

    print an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys as s
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        print("Exception: {}".format(s.exc_info()[1]), file=s.stderr)
+        return False
