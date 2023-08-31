ffea7fe654153e0a4002fa40943c64888cab133d
thatiphele <thatayaonephele@gmail.com>
2023-08-31 09:06:55 -0700

    ByteCode -> Python #1

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import dis
-    from magic import magic_calculation
-
-
-dis.dis(magic_calculation)
+def magic_calculation(a, b):
+    return (98 + a ** b)
