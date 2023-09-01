e830f2502cedb974543446a3ac308b9fb7612f46
thatiphele <thatayaonephele@gmail.com>
2023-09-01 01:56:08 -0700

    Hot Fix

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,29 +1,29 @@
 #include "lists.h"
 /**
- *insert_node - A function that inserts a number into a sorted singly linked list
- *@head: A ptr pointing the head of the sorted singly linked list
+ *insert_node - Inserts a number into a sorted singly linked list
+ *@h: A ptr pointing the head of the sorted singly linked list
  *@number: The number parameter to be inserted
  *Return: A ptr pointing to a temporarary "new" node
  *else NULL on failure
  */
-listint_t *insert_node(listint_t **head, int number)
+listint_t *insert_node(listint_t **h, int number)
 {
-	listint_t *new = *head, *tmp;
+	listint_t *tmp = *h, *new;
 
-	tmp = malloc(sizeof(listint_t)); /*dynamic memory allocation*/
-	if (tmp == NULL) /*test for malloc failure*/
+	new = malloc(sizeof(listint_t)); /*dynamic memory allocation*/
+	if (new == NULL) /*test for malloc failure*/
 		return (NULL);
-	tmp->n = number; /*define data int number*/
-	if (new == NULL || new->n >= number)
+	new->n = number; /*define data int number*/
+	if (tmp->n >= number || tmp == NULL)
 	{
-		tmp->next = new;
-		*head = tmp;
-		return (tmp);
+		new->next = tmp;
+		*h = new;
+		return (new);
 	}
-	while (new && new->next && new->next->n < number) /*iterate*/
-		new = new->next; /*new to next ptr of node*/
-	tmp->next = new->next; /*tmp to next ptr of new*/
-	new->next = tmp; /*next ptr of new eq tmp node*/
+	while (tmp && tmp->next && tmp->next->n < number) /*iterate*/
+		tmp = tmp->next; /*new to next ptr of node*/
+	new->next = tmp->next; /*tmp to next ptr of new*/
+	tmp->next = new; /*next ptr of new eq tmp node*/
 
-	return (tmp);
+	return (new);
 }
