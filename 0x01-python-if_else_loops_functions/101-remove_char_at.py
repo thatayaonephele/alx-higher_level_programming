b9b8ceab7a003741966f71e6abadbc9274b5bf20
thatiphele <thatayaonephele@gmail.com>
2023-09-01 02:40:09 -0700

    Hot Fix

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Remove at position"""
+
 
 def remove_char_at(str, n):
+    """create a copy of the string, remove char at the position n"""
+
     a = str[:n]
     b = str[n + 1:]
     if (n >= 0) is True:
