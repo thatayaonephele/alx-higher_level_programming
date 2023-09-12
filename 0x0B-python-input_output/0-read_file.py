f6c76e62d46d646e624d69e39a94775f8743b2d7
thatiphele <thatayaonephele@gmail.com>
2023-09-12 08:00:54 -0700

    Hot Fix:wq

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,9 @@
 
 
 def read_file(filename=""):
+    """
+    A function that reads in data and prints to stdout
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
             print(line, end="")
