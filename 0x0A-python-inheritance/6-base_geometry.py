4e182e91c09158ab2dab98cc24dcfb297873d6da
thatiphele <thatayaonephele@gmail.com>
2023-09-14 12:27:08 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module that defines the class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """A function that returns Public instance method implementation status"""
+
+    def area(self):
+        """A function that defines the area method'"""
+        raise Exception("area() is not implemented")
