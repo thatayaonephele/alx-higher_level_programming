94284172b21300d2492794026e3612683dac6e2e
thatiphele <thatayaonephele@gmail.com>
2023-09-14 07:23:10 -0700

    Hot Fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A function that lists avail attr & methods of an object"""
 
-def lookup(obj):
 
+def lookup(obj):
 
     """Return: A lookup function returning an object's list"""
     x = dir(obj)
