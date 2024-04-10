f6ca1b0652521b53cd2b1d8d45a9f929ae7938d2
thatayaonephele <thatayaonephele@gmail.com>
2024-04-10 05:20:06 -0700

    Hot Fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-'''Defines a square
-'''
+"""A function that defines addition of integers."""
 
 
 def add_integer(a, b=98):
-    '''Executes the sum of 2 integers.
-    Args:
-        b (int, optional): The second number.
-        a (int): The first number.
-    Return:
-        The sum of the two integers.
-    '''
-    if type(a) != int or float:
-        raise TypeError('a must be an integer')
-    elif type(b) != int or float:
-        raise TypeError('b must be an integer')
-    else:
-        return int(a) + int(b)
+    """Execute the addition of b and a in integer format.
+    Typecast all arguments of float type to integer prior to adding them together.
+    Raises:
+        TypeError: If b or a is a non-float and non-integer type.
+    """
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    return (int(b) + int(a))
