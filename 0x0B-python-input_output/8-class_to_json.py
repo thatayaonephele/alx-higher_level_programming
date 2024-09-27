494ccd76a0aab649eeb6709e9234d7a550a3df77
thatayaonephele <thatayaonephele@gmail.com>
2024-09-26 19:13:33 -0700

    Hot Fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""Module defining MyClass.
+"""Module representing a class named MyClass.
 """
 
 class MyClass:
-    """Representation of MyClass.
+    """Class definition for MyClass with attributes.
     """
 
     def __init__(self, name):
-        self.name = name
-        self.number = 0
+        """Initialize an instance of MyClass with a name and a default number.
+        
+        Args:
+            name (str): The name to assign to the instance.
+        """
+        self.name = name  # Public attribute for the name
+        self.number = 0   # Public attribute for the number
 
     def __str__(self):
-        # Returning a string representation of MyClass
-        return f"[MyClass] {self.name} - {self.number:d}"
-
+        """Return a string representation of the MyClass instance.
+        
+        Returns:
+            str: A formatted string displaying the name and number.
+        """
+        return "[MyClass] {} - {:d}".format(self.name, self.number)
