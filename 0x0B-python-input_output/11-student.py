d46578c04a77bf0518ac09495a8dc01d47caa1eb
thatayaonephele <thatayaonephele@gmail.com>
2024-09-26 22:46:44 -0700

    Hot Fix

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,19 +6,19 @@ class Student:
     ''' A class that defines student'''
     def __init__(self, first_name, last_name, age):
         '''Definition of constructor method'''
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        self.first_name = first_name
 
     def to_json(self, attrs=None):
         '''A function that retrieves a dictionary representation of student'''
-        y = type(attrs)
-        if y is list and all([type(x) != str for x in attrs]):
+        if attrs is None:
             return self.__dict__.copy()
-        else:
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
             return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__.copy()
 
     def reload_from_json(self, json):
-        '''A function tha loads attributes and methods from json'''
+        '''A function that loads attributes and methods from json'''
         for k, v in json.items():
             self.__dict__[k] = v
