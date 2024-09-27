256ec60cf5e249c66616bb0164f35f42b6bbcc42
thatayaonephele <thatayaonephele@gmail.com>
2024-09-26 19:18:27 -0700

    Hot Fix

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-"""Module representing a class named MyClass.
+"""Module to convert class instances to JSON serializable dictionaries.
 """
 
-class MyClass:
-    """Class definition for MyClass with attributes.
+def class_to_json(obj):
+    """Return the dictionary description for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a class.
+
+    Returns:
+        dict: A dictionary representation of the object's attributes.
     """
-
-    def __init__(self, name):
-        """Initialize an instance of MyClass with a name and a default number.
-        
-        Args:
-            name (str): The name to assign to the instance.
-        """
-        self.name = name  # Public attribute for the name
-        self.number = 0   # Public attribute for the number
-
-    def __str__(self):
-        """Return a string representation of the MyClass instance.
-        
-        Returns:
-            str: A formatted string displaying the name and number.
-        """
-        return "[MyClass] {} - {:d}".format(self.name, self.number)
+    return obj.__dict__
